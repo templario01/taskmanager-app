831ea570ff14589b574aac3a4d01c6364cf122e7
Alexander Angles <alexanderaaap@gmail.com>
2022-05-03 21:25:29 -0500

    refact: migration to react native

--- components/SwitchButtons.tsx
+++ components/SwitchButtons.tsx
@@ -1,10 +1,39 @@
 import "../styles/styles.css";
+import { StyleSheet, View, Button, Alert } from 'react-native';
+
+const switchButtons = StyleSheet.create({
+  mainButtons: {
+    display: "flex",
+    justifyContent: "space-around",
+    backgroundColor: "#3c1f7b",
+    borderRadius: 4,
+    alignItems: "center",
+    width: "298px",
+  },
+  mainButton: {
+    background: "transparent",
+    border: "none",
+    height: "40%",
+    borderRadius: 4,
+    fontFamily: "Nunito",
+    fontStyle: "normal",
+    fontweight: "600",
+    fontSize: 16,
+    color: "#ffffff",
+    width: "133%",
+  },
+  btnActive: {
+      backgroundColor: "#272430",
+      height: "32px",
+      width: "133px",
+  }
+})
 
 export function SwitchButtons() {
   return (
-    <div className="main-buttons">
-      <button className="main-button btn-active">Schedule</button>
-      <button className="main-button">Note</button>
-    </div>
+    <View style = {switchButtons.mainButtons}>
+      <Button onPress={() => Alert.alert('Left button pressed')} title="Schedule"/>
+      <Button onPress={() => Alert.alert('Left button pressed')} title="Note"/>
+    </View>
   );
 }
